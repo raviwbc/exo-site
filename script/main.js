e4405bb978904fe1e00f043c6e2b33b0b921e0de
raviwbc <sraviwbc@gmail.com>
2025-09-02 12:02:19 +0530

    check

--- script/main.js
+++ script/main.js
@@ -1,90 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="custom_style.css">
-    <script>
-      window.tailwind = {
-        config: {
-          darkMode: "class",
-         theme: {
-        extend: {
-          colors: {
-            background: 'hsl(var(--background))',
-            foreground: 'hsl(var(--foreground))',
-            card: { DEFAULT: 'hsl(var(--card))', foreground: 'hsl(var(--card-foreground))' },
-            popover: { DEFAULT: 'hsl(var(--popover))', foreground: 'hsl(var(--popover-foreground))' },
-            primary: { DEFAULT: 'hsl(var(--primary))', foreground: 'hsl(var(--primary-foreground))' },
-            secondary: { DEFAULT: 'hsl(var(--secondary))', foreground: 'hsl(var(--secondary-foreground))' },
-            muted: { DEFAULT: 'hsl(var(--muted))', foreground: 'hsl(var(--muted-foreground))' },
-            accent: { DEFAULT: 'hsl(var(--accent))', foreground: 'hsl(var(--accent-foreground))' },
-            destructive: { DEFAULT: 'hsl(var(--destructive))', foreground: 'hsl(var(--destructive-foreground))' },
-            border: 'hsl(var(--border))',
-            input: 'hsl(var(--input))',
-            ring: 'hsl(var(--ring))',
-            chart: {
-              1: 'hsl(var(--chart-1))',
-              2: 'hsl(var(--chart-2))',
-              3: 'hsl(var(--chart-3))',
-              4: 'hsl(var(--chart-4))',
-              5: 'hsl(var(--chart-5))'
-            }
-          },
-          borderRadius: {
-            lg: 'var(--radius)',
-            md: 'calc(var(--radius) - 2px)',
-            sm: 'calc(var(--radius) - 4px)'
-          }
-        }
-      }
-        }
-      }
-    </script>
-
-  </head>
-  <body>
-    <div class="min-h-screen bg-background">
-      <span id="tapbar"></span>
-      <main>
-        <span id="hero"></span>
-        <span id="solution"></span>
-        <span id="clients"></span>
-      </main>
-      <span id="footer"></span>
-    </div>
-    <script>
-      fetch("components/hero.html")
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("hero").innerHTML = data;
-        });
-
-      fetch("components/tabnav.html")
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("tapbar").innerHTML = data;
-        });
-      fetch("components/solution.html")
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("solution").innerHTML = data;
-        });
-      fetch("components/clients.html")
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("clients").innerHTML = data;
-        });
-      fetch("components/footer.html")
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("footer").innerHTML = data;
-        });
-
-      
       let solutions = [
         {
           icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map h-8 w-8 text-primary" aria-hidden="true"><path d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"></path><path d="M15 5.764v15"></path><path d="M9 3.236v15"></path></svg>`,
@@ -239,12 +152,14 @@
             </div>
             `;
       });
-      
 
-      setTimeout(() => {
-        document.getElementById("cardData").innerHTML = htmpList;
-      }, 100);
-    </script>
-    <script src="./script/pageLoader.js"></script>
-  </body>
-</html>
+      function recursiveCheck(){
+        let card = document.getElementById("cardData");
+        if(card){
+            card.innerHTML = htmpList
+        }else{
+            setTimeout(()=>{
+                recursiveCheck();
+            }, 20)
+        }
+      }
