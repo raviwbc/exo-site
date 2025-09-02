59953890512c7b6ac00ec7f599ca8746322e6da6
raviwbc <sraviwbc@gmail.com>
2025-09-02 17:53:42 +0530

    menu update

--- script/pageLoader.js
+++ script/pageLoader.js
@@ -1,1 +1,24 @@
 console.log(window.location.pathname)
+
+function  menuActive(){
+    let path = window.location.pathname;
+    let tagname = document.getElementsByTagName("nav");
+    console.log(tagname.length);
+    if (tagname.length == 0) {
+        setTimeout(()=>{
+            menuActive();
+        },10)
+    } else {
+        let newList = tagname[0].getElementsByTagName('a');
+        console.log(newList)
+        Array.from(newList).forEach((element) => {
+            console.log(element.getAttribute("menu"));
+            if(path.includes(element.getAttribute("menu"))){
+                element.classList.add('active');
+            }else{
+                element.classList.remove('active')
+            }
+        });
+    }
+}
+menuActive();
