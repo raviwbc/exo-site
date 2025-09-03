d3544674cf4c80cba1372f66865af7d713aeab01
raviwbc <sraviwbc@gmail.com>
2025-09-03 10:25:43 +0530

    mail fixes

--- script/contact.js
+++ script/contact.js
@@ -22,40 +22,41 @@
       }, 5000); // hide after 5 seconds
     }
 
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault(); // stop normal form submit (page reload)
+// document.getElementById("contactForm").addEventListener("submit", function(e) {
+//     e.preventDefault(); // stop normal form submit (page reload)
 
-      const btn = document.getElementById('submitBtn');
-  const messageBox = document.getElementById('formMessage');
+//       const btn = document.getElementById('submitBtn');
+//   const messageBox = document.getElementById('formMessage');
 
-  btn.disabled = true;
-  btn.textContent = 'Sending...';
+//   btn.disabled = true;
+//   btn.textContent = 'Sending...';
 
-    const form = e.target;
-    const formData = new FormData(form);
+//     const form = e.target;
+//     const formData = new FormData(form);
 
-    fetch("send.php", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-          btn.disabled = false;
-        const responseDiv = document.getElementById("formResponse");
-        if (data.status === "send") {
-            responseDiv.textContent = data.message;
-            responseDiv.style.color = "green";
-            form.reset(); // optional: clear form after success
-        } else {
-            responseDiv.textContent = "Something went wrong!";
-            responseDiv.style.color = "red";
-        }
-    })
-    .catch(error => {
-        document.getElementById("formResponse").textContent = "Error: " + error;
-        document.getElementById("formResponse").style.color = "red";
-    });
-});
+//     fetch("send.php", {
+//         method: "POST",
+//         body: formData
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       debugger
+//           btn.disabled = false;
+//         const responseDiv = document.getElementById("formResponse");
+//         if (data.status === "send") {
+//             responseDiv.textContent = data.message;
+//             responseDiv.style.color = "green";
+//             form.reset(); // optional: clear form after success
+//         } else {
+//             responseDiv.textContent = "Something went wrong!";
+//             responseDiv.style.color = "red";
+//         }
+//     })
+//     .catch(error => {
+//         document.getElementById("formResponse").textContent = "Error: " + error;
+//         document.getElementById("formResponse").style.color = "red";
+//     });
+// });
 
  document.getElementById("contactForm").addEventListener("submit", function(e) {
       e.preventDefault();
